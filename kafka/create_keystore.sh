e7afa940991e680ef9eb5d65db79175a67ebeed3
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2023-01-11 08:06:31 +0000

    Removed plaintext listener

--- kafka/create_keystore.sh
+++ kafka/create_keystore.sh
@@ -15,13 +15,16 @@ rm -rf $dir
 mkdir -p $dir
 
 # Create a private key
-keytool -genkey -validity $days -alias $alias -dname "C=GB, O=UK Home Office, CN=Callisto $alias" -keystore $dir/$alias.keystore.jks -keyalg RSA  -storepass $password  -keypass $password
+keytool -genkey -validity $days -alias $alias -dname "C=GB, O=UK Home Office, CN=Callisto $alias" -keystore $dir/$alias.keystore.jks -keyalg RSA  -storepass $password  -keypass $password -ext SAN=dns:$alias
  
 # Create CSR
-keytool -keystore $dir/$alias.keystore.jks -alias $alias -certreq -file $dir/$alias.csr -storepass $password -keypass $password
-  
+keytool -keystore $dir/$alias.keystore.jks -alias $alias -certreq -file $dir/$alias.csr -storepass $password -keypass $password -ext SAN=dns:alias
+
+# Create extensions file for SAN
+printf "[SAN]\nsubjectAltName=DNS:%s" $alias  > $dir/ext.cnf
+
 # Create cert signed by CA
-openssl x509 -req -CA ./ca.crt -CAkey ./ca.key -in $dir/$alias.csr -out $dir/$alias-ca-signed.crt -days $days -CAserial $dir/ca.srl -CAcreateserial -passin pass:$password
+openssl x509 -req -CA ./ca.crt -CAkey ./ca.key -in $dir/$alias.csr -out $dir/$alias-ca-signed.crt -days $days -CAserial $dir/ca.srl -CAcreateserial -passin pass:$password -extfile $dir/ext.cnf -extensions SAN
  
 # Import CA cert into keystore
 keytool -keystore $dir/$alias.keystore.jks -alias Callisto -import -noprompt -file ./ca.crt -storepass $password -keypass $password
