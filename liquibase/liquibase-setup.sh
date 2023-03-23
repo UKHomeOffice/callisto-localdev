25a25fe12b26561e4dd1bb06cba49dc21869efc3
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-03-23 11:52:51 +0000

    Fail DB migration when DB schema name isn't specified

--- liquibase/liquibase-setup.sh
+++ liquibase/liquibase-setup.sh
@@ -3,6 +3,12 @@ set -e
 
 db_schema=$1
 
+if [ -z "${db_schema}" ] ; then
+  echo 'Database schema name must be specified as a command argument in Docker compose file'
+  echo 'e.g. command: ["timecard"]'
+  exit 1
+fi
+
 liquibase \
 --url=jdbc:postgresql://postgres:5432/${DATABASE_NAME} \
 --username=${DATABASE_USERNAME} \
