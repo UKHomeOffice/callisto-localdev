4ffa53bccbdfdb4c022e4529ea12ca33e2217821
Taoufik Mohdit <taoufik.mohdit@digital.homeoffice.gov.uk>
2023-03-23 11:39:59 +0000

    Pass on liquibaseSchemaName as a command argument instead of an environment variable for consistency

--- liquibase/liquibase-setup.sh
+++ liquibase/liquibase-setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -e
+
+db_schema=$1
+
 liquibase \
 --url=jdbc:postgresql://postgres:5432/${DATABASE_NAME} \
 --username=${DATABASE_USERNAME} \
 --password=${DATABASE_PASSWORD} \
 --changeLogFile=changelog/db.changelog-main.yml \
---liquibaseSchemaName=${databaseSchemaName} \
-update \
+--liquibaseSchemaName=${db_schema} \
+update
